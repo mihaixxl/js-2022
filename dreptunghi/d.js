a3e2ad24c6c8dd6a1fe37ec61861f88e40fb41ad
Ana Miron <anamiron@MacBook-Air-Ana.local>
2022-02-03 13:53:21 +0200

    cerc

--- dreptunghi/d.js
+++ dreptunghi/d.js
@@ -1,6 +1,8 @@
 var dr = document.querySelector('#dreptunghi')
+var cerc =document.querySelector('#cerc')
 
 dr.addEventListener('click', minimize)
+cerc.addEventListener('click',move )
 
 
 async function minimize() {
@@ -9,4 +11,10 @@ async function minimize() {
         dr.style.height = i + 'em'
     }
     
+}
+async function move() {
+    for(let i = 10; i <= 700; i++ ) {
+        await new Promise(r => setTimeout(r,20))
+        cerc.style.top = cerc.style.left = i + 'px'
+    }
 }
